9ac12752a199bb576f4a85c093ba012efb4532de
typeofMax <work@typeofmax.ru>
2024-03-18 21:56:06 +0300

    Complete variables and data types

---
+++ 2-variables-and-data-types/task-6-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    b = b + a;
+    a = b - a;
+    b = b - a;
+
+    cout << a << " " << b << endl;
+
+    return 0;
+}
---
+++ 2-variables-and-data-types/task-7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    long long x;
+    cin >> x;
+
+    cout << x;
+
+    return 0;
+
+}
